3e5d135dda64eae025b8c52dcc2c839c3db2b705
vladAstahov <asl.digital@mail.ru>
2025-01-04 16:00:16 +0300

    add button to main page

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,11 +1,19 @@
 import {ViewerInfo} from "@/widgets/viewer"
 import {EsimList} from "@/widgets/esim"
 
+import {Button} from "@/shared/ui/Button"
+
 import styles from './MainPage.module.scss'
 
 export const MainPage = () => (
     <div className={styles.root}>
         <ViewerInfo />
         <EsimList />
+        <Button
+            className={styles.button}
+            onClick={() => {}}
+        >
+            Order eSIM
+        </Button>
     </div>
 )
